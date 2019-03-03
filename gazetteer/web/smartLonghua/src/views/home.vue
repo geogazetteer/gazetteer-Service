97a01893e5c4191b8e64a08563489ae822cb5a3b
8ea_100007979546_seniorland <8ea_100007979546_seniorland@git.cloud.tencent.com>
2019-03-03 21:12:58 +0800

    @李颖20190303 smartLonghua 1.0.0

---
+++ gazetteer/web/smartLonghua/src/views/home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wrapper">
+   <base-map :markerCoord="markerCoord"></base-map>
+    <!--搜索框-->
+    <search-box @setMarkCoord="onGetMarkCoord"></search-box>
+
+    <!--右侧工具栏-->
+    <right-tool></right-tool>
+  </div>
+</template>
+
+<script>
+  import baseMap from '../components/baseMap.vue'
+  import searchBox from '../components/searchBox.vue'
+  import rightTool from '../components/rightToolBar.vue'
+export default {
+  name: 'home',
+  components: {
+    baseMap,searchBox,rightTool
+  },
+  data () {
+    return {
+      markerCoord:[]
+    }
+  },
+  methods:{
+    onGetMarkCoord(coordArr){
+      this.markerCoord = coordArr
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.wrapper{
+  width: 100%;
+  height: 100%;
+}
+</style>
