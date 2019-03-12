31a495e80694e79d062bcd958e48f663517ec803
Clover0315 <2361307377@qq.com>
2019-03-13 02:12:33 +0800

    @clover添加地名地址服务列表 urlhttp://localhost:8087/#/AddressWebService

--- gazetteer/web/smartLonghua/src/router/index.js
+++ gazetteer/web/smartLonghua/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import home from '@/views/home.vue'
+import webService from '@/views/webService.vue'
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: home
-    }
+    },
+    {
+      path: '/AddressWebService',
+      name: 'webService',
+      component: webService
+    },
   ]
 })
---
+++ gazetteer/web/smartLonghua/src/views/webService.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="wrapper">
+    <h1 class="title">深圳市龙华区地名地址服务</h1>
+    <Table
+      :columns="columns" :data="data"
+      stripe
+    ></Table>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'webService',
+  components: {
+  },
+  created(){
+
+  },
+  data () {
+    return {
+      columns: [
+        {
+          title:' ',
+          key:'id',
+          width:30,
+          fixed:true
+        },
+        {
+          title: '服务名称',
+          key: 'name',
+		  className:'header'
+        },
+        {
+          title: '服务描述',
+          key: 'describe',
+		  className:'header'
+        },
+        {
+          title: '服务地址URL',
+          key: 'url',
+		  className:'header'
+        }
+      ],
+      data: [
+        {
+          id:1,
+          name: '地名地址服务',
+          describe: '深圳市龙华区地名地址服务',
+          url: '/address/services',
+        },
+        {
+          id:2,
+          name: '综合搜索',
+          describe: '综合查询数据',
+          url: '/address/select',
+        },
+        {
+          id:3,
+          name: '根据建筑物Code查询信息',
+          describe: '实现根据建筑物Code查询相关建筑物信息的服务',
+          url: '/address/selectByCodes/{code}',
+        },
+        {
+          id:4,
+          name: '根据建筑物Address查询信息',
+          describe: '实现根据建筑物Address查询相关建筑物信息的服务',
+          url: '/address/selectByAddress/{address}',
+        }
+      ]
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.wrapper{
+  width: 90%;
+  height: 100%;
+  margin: 0 auto;
+}
+  .title{
+    margin-bottom: 20px;
+  }
+.wrapper .header{
+	font-weight: bold;
+    font-size: 16px;
+  }
+</style>
