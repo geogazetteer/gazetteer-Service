404ca722242f771a8690cdf5a3c987f6c976b408
geodyj <whudyj2010@hotmail.com>
2019-03-14 15:11:37 +0800

    增加mybatis支持

---
+++ gazetteer/database/src/main/java/top/geomatics/gazetteer/database/AddressMapper.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package top.geomatics.gazetteer.database;
+
+import java.util.List;
+
+import top.geomatics.gazetteer.model.AddressRow;
+
+/**
+ * @author whudyj
+ *
+ */
+public interface AddressMapper {
+	public List<AddressRow> selectAddressById(String id) throws Exception;
+
+}
--- gazetteer/model/src/main/java/top/geomatics/gazetteer/model/AddressRow.java
+++ gazetteer/model/src/main/java/top/geomatics/gazetteer/model/AddressRow.java
@@ -1,7 +1,5 @@
 package top.geomatics.gazetteer.model;
 
-import java.sql.Date;
-
 public class AddressRow {
 	private String address_id; // 地址标识，数据库自动生成，示例：63EEDE6BA4206A3AE0538CC0C0C07BB0
 	private String code; // 地址编码，唯一，由权威机构确定，示例：44030600960102T0117 000000 共25位
@@ -14,156 +12,121 @@ public class AddressRow {
 	private String community; // 社区名称
 	private String road; // 路名称
 	private String road_num; // 路牌编号
-	private String villige; // 村名称
-	private String building_name; // 建筑名称
+	private String village; // 村名称
+	private String building; // 建筑名称
 	private String floor; // 楼层
-	private String address_name; // 完整的地址名称
-	private Date update_date; // 地址更新日期
-	private Date create_date; // 地址创建日期
-	private boolean isPublished; // 地址是否已发布
-
+	private String address; // 完整的地址名称
+	private String update_address_date; // 地址更新日期
+	private String publish; // 地址是否已发布
+	private String create_address_date; // 地址创建日期
+	
 	public String getAddress_id() {
 		return address_id;
 	}
-
 	public void setAddress_id(String address_id) {
 		this.address_id = address_id;
 	}
-
 	public String getCode() {
 		return code;
 	}
-
 	public void setCode(String code) {
 		this.code = code;
 	}
-
 	public String getBuilding_id() {
 		return building_id;
 	}
-
 	public void setBuilding_id(String building_id) {
 		this.building_id = building_id;
 	}
-
 	public String getHouse_id() {
 		return house_id;
 	}
-
 	public void setHouse_id(String house_id) {
 		this.house_id = house_id;
 	}
-
 	public String getProvince() {
 		return province;
 	}
-
 	public void setProvince(String province) {
 		this.province = province;
 	}
-
 	public String getCity() {
 		return city;
 	}
-
 	public void setCity(String city) {
 		this.city = city;
 	}
-
 	public String getDistrict() {
 		return district;
 	}
-
 	public void setDistrict(String district) {
 		this.district = district;
 	}
-
 	public String getStreet() {
 		return street;
 	}
-
 	public void setStreet(String street) {
 		this.street = street;
 	}
-
 	public String getCommunity() {
 		return community;
 	}
-
 	public void setCommunity(String community) {
 		this.community = community;
 	}
-
 	public String getRoad() {
 		return road;
 	}
-
 	public void setRoad(String road) {
 		this.road = road;
 	}
-
 	public String getRoad_num() {
 		return road_num;
 	}
-
 	public void setRoad_num(String road_num) {
 		this.road_num = road_num;
 	}
-
-	public String getVillige() {
-		return villige;
+	public String getVillage() {
+		return village;
 	}
-
-	public void setVillige(String villige) {
-		this.villige = villige;
+	public void setVillage(String village) {
+		this.village = village;
 	}
-
-	public String getBuilding_name() {
-		return building_name;
+	public String getBuilding() {
+		return building;
 	}
-
-	public void setBuilding_name(String building_name) {
-		this.building_name = building_name;
+	public void setBuilding(String building) {
+		this.building = building;
 	}
-
 	public String getFloor() {
 		return floor;
 	}
-
 	public void setFloor(String floor) {
 		this.floor = floor;
 	}
-
-	public String getAddress_name() {
-		return address_name;
+	public String getAddress() {
+		return address;
 	}
-
-	public void setAddress_name(String address_name) {
-		this.address_name = address_name;
+	public void setAddress(String address) {
+		this.address = address;
 	}
-
-	public Date getUpdate_date() {
-		return update_date;
+	public String getUpdate_address_date() {
+		return update_address_date;
 	}
-
-	public void setUpdate_date(Date update_date) {
-		this.update_date = update_date;
+	public void setUpdate_address_date(String update_address_date) {
+		this.update_address_date = update_address_date;
 	}
-
-	public Date getCreate_date() {
-		return create_date;
+	public String getPublish() {
+		return publish;
 	}
-
-	public void setCreate_date(Date create_date) {
-		this.create_date = create_date;
+	public void setPublish(String publish) {
+		this.publish = publish;
 	}
-
-	public boolean isPublished() {
-		return isPublished;
+	public String getCreate_address_date() {
+		return create_address_date;
 	}
-
-	public void setPublished(boolean isPublished) {
-		this.isPublished = isPublished;
+	public void setCreate_address_date(String create_address_date) {
+		this.create_address_date = create_address_date;
 	}
 
 }
