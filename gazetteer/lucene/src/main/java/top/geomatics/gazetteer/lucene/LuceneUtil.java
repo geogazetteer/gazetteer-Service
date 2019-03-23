917fe773a40f5c3437f9206e8e30039ccde8d9ca
geodyj <whudyj2010@hotmail.com>
2019-03-23 22:17:08 +0800

    增加了空间查询

--- gazetteer/lucene/src/main/java/top/geomatics/gazetteer/lucene/LuceneUtil.java
+++ gazetteer/lucene/src/main/java/top/geomatics/gazetteer/lucene/LuceneUtil.java
@@ -2,7 +2,6 @@ package top.geomatics.gazetteer.lucene;
 
 import java.io.File;
 import java.io.IOException;
-import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
---
+++ gazetteer/service/address-service/src/main/java/top/geomatics/gazetteer/service/address/BuildingController.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package top.geomatics.gazetteer.service.address;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSON;
+
+import top.geomatics.gazetteer.model.BuildingRow;
+import top.geomatics.gazetteer.utilities.database.excel2gpkg.GeopackageReader;
+
+/**
+ * @author whudyj
+ *
+ */
+@RestController
+@RequestMapping("/building")
+public class BuildingController {
+	String addressFileName = "D:\\data\\LH_gazetteer.gpkg";
+	GeopackageReader reader2 = new GeopackageReader(addressFileName);
+	
+	//reader2.close();
+	
+	/**
+	 * example: http://localhost:8080/building/query?x=503361.375&y=2506786.75
+	 * 
+	 * @param tablename
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	@GetMapping("/query")
+	public String query(
+			@RequestParam(value = "tablename", required = false, defaultValue = "龙华区楼栋") String tablename,
+			@RequestParam(value = "x", required = true) Double x,
+			@RequestParam(value = "y", required = true) Double y) {
+		reader2.preQuery("龙华区楼栋");
+		List<BuildingRow> rows = reader2.query2(x, y);
+		// 使用阿里巴巴的fastjson
+		return JSON.toJSONString(rows);
+	}
+
+}
