1b126c4f61981166063604ecd48767f840bbe607
geodyj <whudyj2010@hotmail.com>
2019-03-24 18:06:37 +0800

    尝试解决spring boot跨域的问题

--- gazetteer/service/address-service/src/main/java/top/geomatics/gazetteer/service/address/AddressServiceApplication.java
+++ gazetteer/service/address-service/src/main/java/top/geomatics/gazetteer/service/address/AddressServiceApplication.java
@@ -3,6 +3,9 @@ package top.geomatics.gazetteer.service.address;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import top.geomatics.gazetteer.database.AddressMapper;
@@ -20,5 +23,5 @@ public class AddressServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AddressServiceApplication.class, args);
 	}
-
+	
 }
--- gazetteer/service/address-service/src/main/java/top/geomatics/gazetteer/service/address/WebMvcConfig.java
+++ gazetteer/service/address-service/src/main/java/top/geomatics/gazetteer/service/address/WebMvcConfig.java
@@ -3,12 +3,22 @@
  */
 package top.geomatics.gazetteer.service.address;
 
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 /**
  * @author whudyj
  *
  */
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+	@Override
+	protected void addCorsMappings(CorsRegistry registry) {
+		super.addCorsMappings(registry);
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+				.maxAge(3600).allowCredentials(true);
+	}
 
 }
