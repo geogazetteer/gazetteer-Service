099348ce269771b24f1f6970ed7ae59a8983bd16
geodyj <whudyj2010@hotmail.com>
2019-03-24 18:05:07 +0800

    Create WebMvcConfig.java

---
+++ gazetteer/service/address-service/src/main/java/top/geomatics/gazetteer/service/address/WebMvcConfig.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package top.geomatics.gazetteer.service.address;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author whudyj
+ *
+ */
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+}
