bee19940873db659ad5719119e134bf19265f602
geodyj <whudyj2010@hotmail.com>
2019-04-01 16:54:23 +0800

    增加了junit 4.11支持

---
+++ gazetteer/utilities/addressUtils/src/test/java/top/geomatics/gazetteer/utilities/address/test/AddressSimilarityTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package top.geomatics.gazetteer.utilities.address.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author whudyj
+ *
+ */
+public class AddressSimilarityTest {
+
+	/**
+	 * Test method for {@link top.geomatics.gazetteer.utilities.address.AddressSimilarity#jaccard(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testJaccard() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link top.geomatics.gazetteer.utilities.address.AddressSimilarity#fuzzy(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testFuzzy() {
+		fail("Not yet implemented");
+	}
+
+}
