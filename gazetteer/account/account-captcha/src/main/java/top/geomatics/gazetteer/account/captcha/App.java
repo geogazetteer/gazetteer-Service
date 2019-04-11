ff2a2264160c9b12a67c10c065c993d5951bc5cb
geodyj <whudyj2010@hotmail.com>
2019-04-11 10:15:22 +0800

    增加用户注册模块

---
+++ gazetteer/account/account-captcha/src/main/java/top/geomatics/gazetteer/account/captcha/App.java
@@ -0,0 +1,13 @@
+package top.geomatics.gazetteer.account.captcha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
