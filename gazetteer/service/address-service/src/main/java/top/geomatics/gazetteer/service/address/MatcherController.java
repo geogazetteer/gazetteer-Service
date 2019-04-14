7026cbc03182613c9ccd94fb72fd53d3b1ad62fd
geoClound <whudyj2010@hotmail.com>
2019-04-14 22:11:41 +0800

    Update MatcherController.java

--- gazetteer/service/address-service/src/main/java/top/geomatics/gazetteer/service/address/MatcherController.java
+++ gazetteer/service/address-service/src/main/java/top/geomatics/gazetteer/service/address/MatcherController.java
@@ -14,6 +14,7 @@ import com.alibaba.fastjson.JSON;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import top.geomatics.gazetteer.model.AddressRow;
 import top.geomatics.gazetteer.model.ComparableAddress;
 import top.geomatics.gazetteer.model.IGazetteerConstant;
@@ -26,7 +27,7 @@ import top.geomatics.gazetteer.utilities.address.AddressSimilarity;
  * 
  * @author whudyj
  */
-@Api(value="/matcher",tags="地名地址匹配服务")
+@Api(value = "/matcher", tags = "地名地址匹配服务")
 @RestController
 @RequestMapping("/matcher")
 public class MatcherController {
@@ -41,12 +42,12 @@ public class MatcherController {
 	 * @param limit    Integer 请求参数，指定返回记录个数
 	 * @return String 返回JSON格式的查询结果
 	 */
-	@ApiOperation(value = "地址匹配", notes = "地址匹配")
+	@ApiOperation(value = "地址匹配", notes = "查询与指定关键词匹配的地址")
 	@GetMapping("/address")
 	public String addressMatcher(
-			@RequestParam(value = IControllerConstant.QUERY_KEYWORDS, required = true, defaultValue = IGazetteerConstant.LH_DISTRICT) String keywords,
-			@RequestParam(value = IControllerConstant.SIMILARITY_MIN, required = false, defaultValue = "0.1") Double min_sim,
-			@RequestParam(value = IControllerConstant.SQL_LIMIT, required = false, defaultValue = "10") Integer limit) {
+			@ApiParam(value = "查询关键词，如 东环一路天汇大厦") @RequestParam(value = IControllerConstant.QUERY_KEYWORDS, required = true, defaultValue = IGazetteerConstant.LH_DISTRICT) String keywords,
+			@ApiParam(value = "最小相似度") @RequestParam(value = IControllerConstant.SIMILARITY_MIN, required = false, defaultValue = "0.2") Double min_sim,
+			@ApiParam(value = "最多匹配个数") @RequestParam(value = IControllerConstant.SQL_LIMIT, required = false, defaultValue = "10") Integer limit) {
 		AddressRow row = new AddressRow();
 		String tablename = "";
 		String shortKeywords = keywords;
@@ -90,8 +91,30 @@ public class MatcherController {
 			}
 		}
 		Collections.sort(sortAddresses);
+		if (sortAddresses.size() >= limit) {
+			sortAddresses = sortAddresses.subList(0, limit);
+		}
 		// </相似性计算，并排序>
 		return ControllerUtils.getResponseBody3(sortAddresses);
+	}
+
+	/**
+	 * <em>度量两个地址的相似度</em><br>
+	 * examples: <br>
+	 * <!--http://localhost:8083/matcher/fuzzy?address1=广东省深圳市龙华区民治街道白石龙社区逸秀新村17栋&address2=深圳市龙华区民治街道白石龙社区白石龙一区58栋58-2
+	 * -->
+	 * 
+	 * @param address1 String 请求参数，指定的标准地址
+	 * @param address2 String 请求参数，指定的待比较（或查询）的地址
+	 * @return String 返回JSON格式的相似度计算结果
+	 */
+	@ApiOperation(value = "度量两个地址的相似度", notes = "相似度指标包括余弦距离、模糊分数、Jaccard相似度和Jaro Winkler 相似度")
+	@GetMapping("/fuzzy")
+	public String addressFuzzy(
+			@ApiParam(value = "标准地址，示例：广东省深圳市龙华区民治街道白石龙社区逸秀新村17栋") @RequestParam(value = "address1", required = true) String address1,
+			@ApiParam(value = "待比较（或查询）的地址，示例：深圳市龙华区民治街道白石龙社区白石龙一区58栋58-2") @RequestParam(value = "address2") String address2) {
+
+		return JSON.toJSONString(AddressSimilarity.indicator(address1, address2));
 	}
 
 }
