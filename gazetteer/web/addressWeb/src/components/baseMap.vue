8a4f486c99358966e4a44428b7aab752a8408c57
Clover0315 <2361307377@qq.com>
2019-04-16 22:02:47 +0800

    @clover 接地图

--- gazetteer/web/addressWeb/src/components/baseMap.vue
+++ gazetteer/web/addressWeb/src/components/baseMap.vue
@@ -5,7 +5,7 @@
 
   <div class='baseMapWrapper'>
     <div id="baseMap"></div><!--地图-->
-
+    <div style="position: absolute;bottom: 10px;left:10px;color:blue;font-size: 20px" id="coords"></div>
   </div>
 
 
@@ -13,11 +13,12 @@
 
 
 <script>
-
-  import * as maptalks from 'maptalks';
+  // import * as maptalks from 'maptalks';
   //marker图标
   import marker from './red_cursor.png'
   import mapboxgl from 'mapbox-gl'
+  import {MAPURL} from '../js/config.js'
+
   export default{
     name: 'baseMap',
     data() {
@@ -42,7 +43,7 @@
 
 
         //地点marker
-        var siteMarker = new maptalks.Marker([0,0],{
+/*        var siteMarker = new maptalks.Marker([0,0],{
           cursor:'pointer',
           "symbol":{
             "markerFile"  : marker,
@@ -68,24 +69,40 @@
             new maptalks.VectorLayer('markerLayer', [siteMarker])
           ]
         });
+
+
         //setCoordinates
         $this.baseMap = map;
-        $this.siteMarker = siteMarker;
-       /* var map = new mapboxgl.Map({
+        $this.siteMarker = siteMarker;*/
+
+/*
+        var map = new mapboxgl.Map({
           container: 'baseMap',
           style: {
             "version": 8,
-//            "scheme": "xyz",
             "sources": {
+              //腾讯底图
+              /!*'tencent_tile':{
+                type:"raster",
+                scheme:'xyz',
+                tiles:[MAPURL['mapTile_tx']]
+              },*!/
               //龙华房屋面
               "longhua": {
                 "type": "vector",
                 "scheme": "tms",
-                "tiles": ["http://localhost:9093/ProxyServlet/proxyHandler?url=http://localhost:9010/geoserver/gwc/service/tms/1.0.0/shenzhen:longhuabuilding/epsg:4326:512@pbf/{z}/{x}/{y}.pbf"],
+                "tiles": [MAPURL['building_vec_tile']],
                 "tileSize": 512
               }
             },
             "layers": [
+              //腾讯底图图层
+             /!* {
+                "id": "tencent_map",
+                "type": "raster",
+                "source": "tencent_tile",
+              },*!/
+              //房屋面边界
               {
                 "id": "border",
                 "type": "line",
@@ -100,6 +117,7 @@
                   "line-width": 2
                 },
               },
+              //房屋面区域
               {
                 "id": "region",
                 "type": "fill",
@@ -114,19 +132,95 @@
               }
             ]
           },
-          center: [0, 0],
+          center: [114.051259,22.702632],
           minZoom: 0,
           maxZoom: 18,
-          zoom: 2
+          zoom: 0
         });
+
 
         map.on('load',function(){
           map.on('click','region',function(e){
             var p = e.features[0].properties;
-            debugger
-            console.log(JSON.stringify(p))
-          })
+            //console.log(JSON.stringify(p))
+          });
+          map.on('mousemove',function (e) {
+            document.getElementById('coords').innerHTML='缩放层级：'+map.getZoom()+'</br>'+e.lngLat.lng+';'+e.lngLat.lat
+          });
         })*/
+
+         var map = new mapboxgl.Map({
+                 container: 'baseMap',
+                 style: {
+                   "version": 8,
+       //            "scheme": "xyz",
+                   "sources": {
+                    //腾讯底图
+                     /*'tencent_tile': {
+                       type: "raster",
+                       scheme: 'xyz',
+                       tiles: [MAPURL['mapTile_tx']]
+                     },*/
+                     //龙华房屋面
+                     "longhua": {
+                       "type": "vector",
+                       "scheme": "tms",
+                       "tiles": ["http://localhost:9093/ProxyServlet/proxyHandler?url=http://localhost:9010/geoserver/gwc/service/tms/1.0.0/shenzhen:longhuabuilding/epsg:4326:512@pbf/{z}/{x}/{y}.pbf"],
+                       "tileSize": 512
+                     }
+                   },
+                   "layers": [
+                     /* {
+                     "id": "tencent_map",
+                      "type": "raster",
+                      "source": "tencent_tile",
+                      },*/
+                     {
+                       "id": "border",
+                       "type": "line",
+                       "source-layer": "longhuabuilding",
+                       "source": "longhua",
+                       "layout": {
+                         "line-join": "round",
+                         "line-cap": "round"
+                       },
+                       "paint": {
+                         "line-color": '#666666',
+                         "line-width": 2
+                       },
+                     },
+                     {
+                       "id": "region",
+                       "type": "fill",
+                       "source-layer": "longhuabuilding",
+                       "source": "longhua",
+                       "layout": {
+
+                       },
+                       "paint": {
+                         'fill-opacity':0
+                       },
+                     }
+                   ]
+                 },
+                 center: [114.051259,22.702632],
+                 minZoom: 0,
+                 maxZoom: 18,
+                 zoom: 2,
+           renderWorldCopies: false,
+           isAttributionControl: false
+               });
+
+               map.on('load',function(){
+                 map.on('click','region',function(e){
+                   var p = e.features[0].properties;
+                   debugger
+                   console.log(JSON.stringify(p))
+                 });
+                 map.on('mousemove',function (e) {
+                   document.getElementById('coords').innerHTML='缩放层级：'+map.getZoom()+'</br>'+e.lngLat.lng+';'+e.lngLat.lat
+                 });
+               })
       },
 
     },
