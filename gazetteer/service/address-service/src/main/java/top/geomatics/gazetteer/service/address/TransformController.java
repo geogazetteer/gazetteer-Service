59b259dd6070f296803d2cbf34011082d29f2578
geodyj <whudyj2010@hotmail.com>
2019-04-20 10:06:31 +0800

    增加输入坐标范围、严重错误地址判断

--- gazetteer/service/address-service/src/main/java/top/geomatics/gazetteer/service/address/TransformController.java
+++ gazetteer/service/address-service/src/main/java/top/geomatics/gazetteer/service/address/TransformController.java
@@ -17,65 +17,61 @@ import top.geomatics.gazetteer.utilities.address.AddressProcessor;
  * @author chenfa
  */
 
-@Api(value="/transform",tags="特殊地址转换处理")
+@Api(value = "/transform", tags = "特殊地址转换处理")
 @RestController
 @RequestMapping("/transform")
 public class TransformController {
-	
+
 	/**
 	 * <em>全角字符转换为半角字符，示例：１2３</em><br>
 	 * examples:<br>
-	 * <!--http://localhost:8083/transform/full?chars=１2３
-	 * -->
+	 * <!--http://localhost:8083/transform/full?chars=１2３ -->
 	 * 
 	 * @param input
 	 * @return
 	 */
 	@ApiOperation(value = "全角转半角", notes = "全角字符转换为半角字符，示例：/transform/full?chars=１2３")
 	@GetMapping("/full")
-    public  String fullToHalf(
-    		@ApiParam(value = "全角字符，如 １2３")
-    		@RequestParam(value = "chars", required = true)  String input) {
-             
-        return JSON.toJSONString(AddressProcessor.fullToHalf(input));
-    }	
-    
+	public String fullToHalf(
+			@ApiParam(value = "全角字符，如 １2３") @RequestParam(value = "chars", required = true) String input) {
+
+		return JSON.toJSONString(AddressProcessor.fullToHalf(input));
+	}
+
 	/**
 	 * <em>繁体汉字转换为简体汉字，示例：龍華</em><br>
 	 * examples:<br>
-	 * <!--http://localhost:8083/transform/complex?chars=龍華
-	 * -->
+	 * <!--http://localhost:8083/transform/complex?chars=龍華 -->
 	 * 
 	 * @param input
 	 * @return
 	 */
 	@ApiOperation(value = "繁体汉字转换为简体汉字", notes = "繁体汉字转换为简体汉字，示例：/transform/complex?chars=龍華")
 	@GetMapping("/complex")
-    public String comToSimple(
-    		@ApiParam(value = "繁体汉字，如 龍華")
-    		@RequestParam(value = "chars", required = true)  String input){
- 		
-		//繁体转中文简体
+	public String comToSimple(
+			@ApiParam(value = "繁体汉字，如 龍華") @RequestParam(value = "chars", required = true) String input) {
+
+		// 繁体转中文简体
 		return JSON.toJSONString(AddressProcessor.comToSimple(input));
 
-   }
-	
+	}
+
 	/**
 	 * <em>汉字数字转换为阿拉伯数字，示例：一,二,三</em><br>
 	 * examples:<br>
 	 * <!--http://localhost:8083/transform/digital?chars=百-->
+	 * 
 	 * @param input
 	 * @return
 	 */
 	@ApiOperation(value = "汉字数字转换为阿拉伯数字", notes = "汉字数字转换为阿拉伯数字，示例：/transform/digital?chars=百")
 	@GetMapping("/digital")
-    public  String chineseToNumber(
-    		@ApiParam(value = "汉字数字，如 百")
-    		@RequestParam(value = "chars", required = true)  String input){
-        
-        return JSON.toJSONString(AddressProcessor.chineseToNumber(input));
-    }
-	
+	public String chineseToNumber(
+			@ApiParam(value = "汉字数字，如 百") @RequestParam(value = "chars", required = true) String input) {
+
+		return JSON.toJSONString(AddressProcessor.chineseToNumber(input));
+	}
+
 	/**
 	 * <em>识别地址中的门牌号，返回门牌号</em><br>
 	 * examples:<br>
@@ -87,12 +83,43 @@ public class TransformController {
 	 */
 	@ApiOperation(value = "识别门牌号", notes = "识别门牌号，示例：/transform/houseNumber?chars=深圳市龙华区大浪街道同胜社区石观工业区19栋3楼")
 	@GetMapping("/houseNumber")
-    public String recognizeHouse(
-    		@ApiParam(value = "地址，如深圳市龙华区大浪街道同胜社区石观工业区19栋3楼")
-    		@RequestParam(value = "chars", required = true)  String input){
-		//繁体转中文简体
+	public String recognizeHouse(
+			@ApiParam(value = "地址，如深圳市龙华区大浪街道同胜社区石观工业区19栋3楼") @RequestParam(value = "chars", required = true) String input) {
 		return JSON.toJSONString(WordSegmenter.houseNumberSegment(input));
 
-   }
-    
+	}
+
+	/**
+	 * <em>判断搜索输入是否含有敏感词</em><br>
+	 * examples:<br>
+	 * <!--http://localhost:8083/transform/sensitive?chars=基地组织 -->
+	 * 
+	 * @param input
+	 * @return
+	 */
+	@ApiOperation(value = "判断搜索输入是否含有敏感词", notes = "判断搜索输入是否含有敏感词，示例：/transform/sensitive?chars=基地组织")
+	@GetMapping("/sensitive")
+	public String isSensitiveWords(
+			@ApiParam(value = "含有敏感词的输入，如基地组织") @RequestParam(value = "chars", required = true) String input) {
+		return JSON.toJSONString(AddressProcessor.isSensitiveWords(input));
+
+	}
+
+	/**
+	 * <em>判断搜索输入是否为正确的坐标</em><br>
+	 * examples:<br>
+	 * <!--http://localhost:8083/transform/coordinate?chars=113.975436877645,22.5995874795642
+	 * -->
+	 * 
+	 * @param input
+	 * @return
+	 */
+	@ApiOperation(value = "判断搜索输入是否为正确的坐标", notes = "判断搜索输入是否为正确的坐标，示例：/transform/coordinate?chars=113.975436877645,22.5995874795642")
+	@GetMapping("/coordinate")
+	public String isCoordinatesExpression(
+			@ApiParam(value = "坐标表达式，如113.975436877645,22.5995874795642") @RequestParam(value = "chars", required = true) String input) {
+		return JSON.toJSONString(AddressProcessor.isCoordinatesExpression(input));
+
+	}
+
 }
