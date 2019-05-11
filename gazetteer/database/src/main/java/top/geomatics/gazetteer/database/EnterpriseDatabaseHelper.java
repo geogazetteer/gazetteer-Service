2a1fa9dc8dc7ff31da445feadf37ba5da3e37f21
geodyj <whudyj2010@hotmail.com>
2019-05-11 13:07:58 +0800

    增加database模块的注释文档、日志、单元测试和国际化处理

--- gazetteer/database/src/main/java/top/geomatics/gazetteer/database/EnterpriseDatabaseHelper.java
+++ gazetteer/database/src/main/java/top/geomatics/gazetteer/database/EnterpriseDatabaseHelper.java
@@ -9,24 +9,33 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
+ * <b>企业法人库操作的帮助类</b><br>
+ * 
  * @author whudyj
  *
  */
 public class EnterpriseDatabaseHelper {
+	// 添加slf4j日志实例对象
+	private final static Logger logger = LoggerFactory.getLogger(EnterpriseDatabaseHelper.class);
 
-	public static SqlSessionFactory sessionFactory;
+	private static final String resource = "enterprise_db_config.xml";
+
+	private static SqlSessionFactory sessionFactory;
 
 	static {
 		try {
 			// 使用MyBatis提供的Resources类加载mybatis的配置文件
-			String resource = "enterprise_db_config.xml";
 			InputStream inputStream = Resources.getResourceAsStream(resource);
 			// 构建sqlSession的工厂
 			sessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
 		} catch (Exception e) {
 			e.printStackTrace();
+			String logMsgString = String.format(Messages.getString("EnterpriseDatabaseHelper.1"), resource); //$NON-NLS-1$
+			logger.error(logMsgString);
 		}
 
 	}
---
+++ gazetteer/database/src/test/java/top/geomatics/gazetteer/database/EnterpriseAddressMapperTest.java
@@ -0,0 +1,58 @@
+package top.geomatics.gazetteer.database;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import top.geomatics.gazetteer.model.EnterpriseRow;
+
+public class EnterpriseAddressMapperTest {
+
+	private static EnterpriseDatabaseHelper edHelper = new EnterpriseDatabaseHelper();
+	private static SqlSession session = edHelper.getSession();
+	private static EnterpriseAddressMapper mapper = session.getMapper(EnterpriseAddressMapper.class);
+
+	@Test
+	public void testSelectByFid() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put(Messages.getString("EnterpriseAddressMapperTest.0"), Messages.getString("EnterpriseAddressMapperTest.1")); //$NON-NLS-1$ //$NON-NLS-2$
+		map.put(Messages.getString("EnterpriseAddressMapperTest.2"), Messages.getString("EnterpriseAddressMapperTest.3")); //$NON-NLS-1$ //$NON-NLS-2$
+		map.put(Messages.getString("EnterpriseAddressMapperTest.4"), 1); //$NON-NLS-1$
+		List<EnterpriseRow> list = mapper.findEquals(map);
+		// 判断查询结果是否正确
+		assertTrue(list.size() == 1);
+		EnterpriseRow row = list.get(0);
+		assertTrue(row.getCode().compareTo(Messages.getString("EnterpriseAddressMapperTest.5")) == 0); //$NON-NLS-1$
+	}
+
+	@Test
+	public void testSelectByFids() {
+	}
+
+	@Test
+	public void testFindEquals() {
+	}
+
+	@Test
+	public void testFindLike() {
+	}
+
+	@Test
+	public void testFindPageEquals() {
+	}
+
+	@Test
+	public void testGetCount() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put(Messages.getString("EnterpriseAddressMapperTest.6"), Messages.getString("EnterpriseAddressMapperTest.7")); //$NON-NLS-1$ //$NON-NLS-2$
+		Integer count = mapper.getCount(map);
+		// 判断查询结果是否正确
+		assertTrue(count == 1576);
+	}
+
+}
