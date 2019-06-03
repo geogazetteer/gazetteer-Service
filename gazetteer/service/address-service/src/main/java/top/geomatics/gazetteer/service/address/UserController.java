6b5b5c033df5dcecaf872e301b291fd2980e6e1a
geodyj <whudyj2010@hotmail.com>
2019-06-03 16:31:22 +0800

    增加用户模块

--- gazetteer/service/address-service/src/main/java/top/geomatics/gazetteer/service/address/UserController.java
+++ gazetteer/service/address-service/src/main/java/top/geomatics/gazetteer/service/address/UserController.java
@@ -1,5 +1,13 @@
 package top.geomatics.gazetteer.service.address;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,30 +16,74 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import springfox.documentation.annotations.ApiIgnore;
+import top.geomatics.gazetteer.model.user.User;
+import top.geomatics.gazetteer.service.user.IUserService;
 
 /**
  * <b>系统用户管理服务类</b><br>
  * 
  * @author whudyj
  */
-@ApiIgnore
 @Api(value = "/user", tags = "系统用户管理服务")
 @RestController
 @RequestMapping("/user")
 public class UserController {
+	@Autowired
+	private IUserService userService;
 
-	@ApiOperation(value = "用户注册", notes = "用户注册，功能暂未实现")
+	@ApiOperation(value = "用户注册", notes = "用户注册")
 	@GetMapping("/register")
-	public String register(@RequestParam String username, @RequestParam String password) {
+	public String register(@RequestParam User user, @RequestParam Model model, BindingResult bindingResult) {
+		if (userService.existUser(user)) {
+			bindingResult.rejectValue("username", "userExist", "用户名已存在");
 
-		return "";
+		}
+		if (bindingResult.hasErrors()) {
+
+			// 获取错误
+			List<ObjectError> errors = bindingResult.getAllErrors();
+			model.addAttribute("errors", errors);
+			model.addAttribute("registuser", user);
+			System.out.println(user.getUsername());
+			return "register";
+		}
+
+		else {
+			userService.register(user);
+			model.addAttribute("logininfo", "注册成功，请登陆！");
+			return "myaccount";
+		}
 	}
 
-	@ApiOperation(value = "用户登录", notes = "用户登录，功能暂未实现")
+	@ApiOperation(value = "用户登录", notes = "用户登录")
 	@GetMapping("/login")
-	public String login(@RequestParam String username, @RequestParam String password) {
+	public String login(@RequestParam User user, @RequestParam HttpSession session, @RequestParam Model model) {
+		User user2 = userService.userLogin(user);
 
-		return "";
+		if (user2 != null) {
+			session.setAttribute("LoginUser", user2);
+			return "forward:index.action";
+		} else {
+			model.addAttribute("error", "loginerror");
+			return "myaccount";
+		}
 	}
 
+	@ApiOperation(value = "用户注销", notes = "用户注销")
+	@GetMapping("/logout")
+	public String logout(@RequestParam HttpSession session) {
+		session.removeAttribute("LoginUser");
+		return "redirect:index.action";
+	}
+
+	@ApiOperation(value = "用户更新", notes = "用户更新")
+	@GetMapping("/update")
+	public String update(@RequestParam User user, @RequestParam HttpSession session, @RequestParam long userid) {
+		user.setUserid(userid);
+		userService.updateUser(user);
+
+		User user2 = userService.selectUserByid(userid);
+		session.setAttribute("LoginUser", user2);
+		return "myaccount";
+	}
 }
