99d2960412d20a7b86c59003ec8409f6c1b464c4
geodyj <whudyj2010@hotmail.com>
2019-07-15 19:31:03 +0800

    修改fastjson循环引用对象检测的问题

--- gazetteer/service/address-service/src/main/java/top/geomatics/gazetteer/service/address/AddressServiceApplication.java
+++ gazetteer/service/address-service/src/main/java/top/geomatics/gazetteer/service/address/AddressServiceApplication.java
@@ -4,6 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -23,6 +29,23 @@ public class AddressServiceApplication {
 		logger.info(logMsgString);
 
 		SpringApplication.run(AddressServiceApplication.class, args);
+	}
+
+	/**
+	 * 使用@Bean注解注入第三方的解析框架（fastJson）
+	 */
+	@Bean
+	public HttpMessageConverters fastJsonHttpMessageConverters() {
+		// 1、首先需要先定义一个convert转换消息对象
+		FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+		// 2、添加fastJson的配置信息，比如：是否要格式化返回的json数据
+		FastJsonConfig fastJsonConfig = new FastJsonConfig();
+		fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+		fastJsonConfig.setSerializerFeatures(SerializerFeature.DisableCircularReferenceDetect);
+		// 3、在convert中添加配置信息
+		fastConverter.setFastJsonConfig(fastJsonConfig);
+
+		return new HttpMessageConverters(fastConverter);
 	}
 
 }
